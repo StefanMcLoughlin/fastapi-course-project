97aaf6e8bde9fdf8b3f1a829b5ddc07a90d6894d
Stefan McLoughlin <meckie1991@gmail.com>
2026-05-30 13:19:37 +0200

    feat: find posts by id in PostgreSQL using psycopg2

--- app/main.py
+++ app/main.py
@@ -60,7 +60,8 @@ def create_posts(post: Post):
 
 @app.get("/posts/{id}")
 def get_post(id: int):
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id)))
+    post= cursor.fetchone()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
     return {"post_detail": post}
