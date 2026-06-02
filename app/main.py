e963627b21de47f735b821fa3fc79e521449f93a
Stefan McLoughlin <meckie1991@gmail.com>
2026-06-02 16:11:09 +0200

    feat: migrate create posts endpoint to SQLAlchemy ORM

--- app/main.py
+++ app/main.py
@@ -46,10 +46,11 @@ def test_posts(db: Session = Depends(get_db)):
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"data": new_post}
 
 @app.get("/posts/{id}")
