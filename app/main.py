023a49549b73981d8539c5b99d882a188d136fde
Stefan McLoughlin <meckie1991@gmail.com>
2026-06-02 19:27:02 +0200

    feat: exclude password from user responses

--- app/main.py
+++ app/main.py
@@ -53,7 +53,7 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     db.commit()
     return post_query.first()
 
-@app.post("/users", status_code=status.HTTP_201_CREATED)
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     new_user = models.User(**user.model_dump())
     db.add(new_user)
--- app/schemas.py
+++ app/schemas.py
@@ -19,3 +19,9 @@ class Post(PostBase):
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    class Config:
+        orm_mode = True
