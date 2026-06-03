2ee448b96df669f2416040454ae5d4bc7453f529
Stefan McLoughlin <meckie1991@gmail.com>
2026-06-03 18:18:01 +0200

    feat: add user login authentication

--- app/utils.py
+++ app/utils.py
@@ -3,3 +3,6 @@ pwd_context = CryptContext(schemes = ["bcrypt"], deprecated="auto")
 
 def hash(password: str):
     return pwd_context.hash(password)
+
+def verify(plain_password, hased_pasword):
+    return pwd_context.verify(plain_password, hased_pasword)
