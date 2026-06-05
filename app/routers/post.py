54f229409ef8b57945bb01f87883ac705116cf4b
Stefan McLoughlin <meckie1991@gmail.com>
2026-06-05 09:47:25 +0200

    feat: add post title search filter

--- app/routers/post.py
+++ app/routers/post.py
@@ -2,7 +2,7 @@ from .. import models, schemas, oauth2
 from fastapi import status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from ..database import get_db
-from typing import List
+from typing import List, Optional
 
 router = APIRouter(
     prefix="/posts",
@@ -11,8 +11,8 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def test_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0):
-    posts = db.query(models.Post).limit(limit).offset(skip).all()
+def test_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str]= ""):
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
