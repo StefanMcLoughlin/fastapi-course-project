211c1b6abdc0898ba6efe196497dc3f56acb844e
Stefan McLoughlin <meckie1991@gmail.com>
2026-06-08 09:59:29 +0200

    feat: add vote count to posts endpoint

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,5 +1,6 @@
 from .. import models, schemas, oauth2
 from fastapi import status, HTTPException, Depends, APIRouter
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from ..database import get_db
 from typing import List, Optional
@@ -10,9 +11,9 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=List[schemas.Post])
-def test_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str]= ""):
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+@router.get("/", response_model=List[schemas.PostOut])
+def get_posts(db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str]= ""):    
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
@@ -25,7 +26,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     return new_post
 
 @router.get("/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def get_post(id: int, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
--- app/schemas.py
+++ app/schemas.py
@@ -25,6 +25,12 @@ class Post(PostBase):
     
     model_config = ConfigDict(from_attributes=True)
 
+class PostOut(BaseModel):
+    Post: Post
+    votes: int
+   
+    model_config = ConfigDict(from_attributes=True)
+
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
