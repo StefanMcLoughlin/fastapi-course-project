ed555c823a2ed9d61825bae249a30dab2b157098
Stefan McLoughlin <meckie1991@gmail.com>
2026-06-16 18:08:29 +0200

    test: add post creation tests

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,5 @@
 from app import schemas
+import pytest
 
 def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
@@ -26,3 +27,34 @@ def test_get_one_post(authorized_client, test_posts):
     assert post.Post.id == test_posts[0].id
     assert post.Post.content == test_posts[0].content
     assert post.Post.title == test_posts[0].title
+    assert res.status_code == 200
+
+
+@pytest.mark.parametrize("title, content, published", [
+    ("awesome new title", "awesome new content", True),
+    ("favorit pizza", "i love magaritha", False),
+    ("tallest skyscraper", "wahoo", True),
+])
+def test_create_post(authorized_client, test_user, title, content, published):
+    res = authorized_client.post("/posts/", json={"title": title, "content": content, "published": published})
+
+    created_post = schemas.Post(**res.json())
+    assert res.status_code == 201
+    assert created_post.title == title
+    assert created_post.content == content
+    assert created_post.published == published
+    assert created_post.owner_id == test_user["id"]
+
+def test_create_post_default_published_true(authorized_client, test_user):
+    res = authorized_client.post("/posts/", json={"title": "arbitrary title", "content": "bla bla bla"})
+
+    created_post = schemas.Post(**res.json())
+    assert res.status_code == 201
+    assert created_post.title == "arbitrary title"
+    assert created_post.content == "bla bla bla"
+    assert created_post.published is True
+    assert created_post.owner_id == test_user["id"]
+
+def test_unauthorized_user_create_post(client):
+    res = client.post("/posts/", json={"title": "arbitrary title", "content": "bla bla bla"})
+    assert res.status_code == 401
